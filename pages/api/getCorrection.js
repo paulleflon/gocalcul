af91911220cde0233b3f56d997a4323ba260d0f5
hickatheworld <cc@hicka.world>
2024-01-28 03:21:38 +0100

    idk what im odign

--- pages/api/getCorrection.js
+++ pages/api/getCorrection.js
@@ -4,14 +4,18 @@ const wa = WolframAlphaAPI(process.env.WOLFRAM_APPID);
 
 export default async function handler(req, res) {
 	try {
-		const response = await wa.getFull("expand " + req.body.formula);
-		for (const pod of response.pods) {
-			const title = pod.title.toLowerCase();
-			if (title === 'result' || title.includes('simplified') || title.includes('expanded')) {
-				res.status(200).json({correction: pod.subpods[0].plaintext});
+		if (req.method === 'POST') {
+			const response = await wa.getFull("expand " + req.body.formula);
+			for (const pod of response.pods) {
+				const title = pod.title.toLowerCase();
+				if (title === 'result' || title.includes('simplified') || title.includes('expanded')) {
+					res.status(200).json({correction: pod.subpods[0].plaintext});
+				}
 			}
+			res.status(400).json({error: true, message: 'Could not automatically generate a correction.'});
+		} else {
+			throw 'error'; //tempfixf
 		}
-		res.status(400).json({error: true, message: 'Could not automatically generate a correction.'});
 	} catch (err) {
 		res.status(500).json({error: true, err});
 	}
