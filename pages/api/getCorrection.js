58835f783d386b37b5b07743e415bd048d5520b5
hickatheworld <cc@hicka.world>
2024-02-04 12:04:10 +0100

    feat(api): prevent WolframAPI calls in development
    In development, the getCorrection api will just return a generic correction after an arbitrary delay.

--- pages/api/getCorrection.js
+++ pages/api/getCorrection.js
@@ -3,6 +3,14 @@ import WolframAlphaAPI from '@wolfram-alpha/wolfram-alpha-api';
 const wa = WolframAlphaAPI(process.env.WOLFRAM_APPID);
 
 export default async function handler(req, res) {
+	if (process.env.DEVELOPMENT === 'true') {
+		const delay = Math.floor(Math.random() * 5000 + 1000);
+		return setTimeout(() => {
+			res.status(200).json({
+		correction: '{-10 x^{9} + 30 x^{6} z^{5} - 16 x^{6} - 7 x^{5} z^{6} + 42 x^{5} z^{5} + 100 y^{2} - 80 y z^{5} - 10 z^{11} + 76 z^{10} + 4 z^{8}}'
+			});
+		}, delay);
+	}
 	try {
 		if (req.method === 'POST') {
 			const response = await wa.getFull("expand " + req.body.formula);
